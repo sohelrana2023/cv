385f6b28506c9d93619a11db4e32e07f2bb2bc7b
sohelrana2023 <130089775+sohelrana2023@users.noreply.github.com>
2023-04-07 01:32:39 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing",{
+    strings:["","Freelancer","Student"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
